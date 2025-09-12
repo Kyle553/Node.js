e8579eaca8daf27994aa875057f7e3cfb611ede2
Kyle553 <elkorda204@gmail.com>
2025-09-12 22:49:19 +0300

    lesson 25-26

---
+++ node.js/lesson-25/script.js
@@ -0,0 +1,3 @@
+console.log("REPL - Read-Eval-Print-Loop")
+console.log("print `node` in console")
+console.log(".help")
---
+++ node.js/lesson-26/script.js
@@ -0,0 +1,4 @@
+const math = require("./math.js");
+
+console.log(math.add(5, 5));
+console.log(math.multiply(2, 2));
