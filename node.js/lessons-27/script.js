1b2079b993c05c1c0938538b798e4027dbebb2b6
Kyle553 <elkorda204@gmail.com>
2025-09-19 00:00:49 +0300

    Partial fix: async callback handling

--- node.js/lessons-27/script.js
+++ node.js/lessons-27/script.js
@@ -4,45 +4,49 @@ const fileText = "test test test";
 let fileData = "";
 const textToAdd = "AAAAAAAAAAAAA";
 
-
-fs.access(fileName, fs.constants.F_OK, (err) => {
-  if (err) {
+function checkOrCreateFile() {
+  try {
+    fs.accessSync(fileName, fs.constants.F_OK)
+  } catch (err) {
     console.error(`The file ${fileName} does not exist:\n`, `ERROR CODE: ${err.code}`);
-
     console.log(`Creating the file ${fileName}`)
-    fs.writeFile(`./${fileName}`, "", (err) => {
-      if (err) {
-        console.error(`Error while creating file ${fileName}`, err);
-        process.exit(1);
-      }
-
-      console.log(`The file ${fileName} created`);
-    })  
-  }
-
-  console.log(`File ${fileName} exists`);
-
-  fs.writeFile(`./${fileName}`, `${fileText}`, (err) => {
-    if (err) {
-      console.error(err);
-      return;
+    try {
+      fs.writeFileSync(`./${fileName}`, "")  
+    } catch (err) {
+      console.error(`Error while creating file ${fileName}`, err);
+      process.exit(1);
     }
+    console.log(`The file ${fileName} created`);
+  }
+  console.log(`File ${fileName} exists`);
+}
 
-    console.log("File overwritten: \n", fileText);
-  })
-})
+checkOrCreateFile();
 
-fs.readFile(`./${fileName}`, "utf8", (err, data) => {
+fs.writeFile(`./${fileName}`, `${fileText}`, (err) => {
   if (err) {
-    console.error("Error reading file: \n", err);
-    process.exit(1);
+    console.error(err);
+    return;
   }
 
-  fileData = data;
-  console.log("File successfully read \n", fileData);
+  console.log("File overwritten: \n", fileText);
 })
 
-fs.appendFile(`./${fileName}`, textToAdd, "utf-8", (err) => {
+function readFile() {
+  fs.readFile(`./${fileName}`, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file: \n", err);
+      process.exit(1);
+    }
+  
+    fileData = data;
+    console.log("File successfully read \n", fileData);
+  })
+}
+
+readFile();
+
+fs.appendFile(`./${fileName}`, "\n" + textToAdd, "utf-8", (err) => {
   if (err) {
     console.error("Error when adding text to a file \n", err);
     process.exit(1);
@@ -50,3 +54,5 @@ fs.appendFile(`./${fileName}`, textToAdd, "utf-8", (err) => {
 
   console.log("Text successfully added to file: \n", textToAdd);
 })
+
+readFile();
