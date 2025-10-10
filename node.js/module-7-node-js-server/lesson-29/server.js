fd3d59a82f0ba7338756f0f419fe36196e032025
Kyle553 <elkorda204@gmail.com>
2025-10-10 15:42:30 +0300

    lesson-29

---
+++ node.js/module-7-node-js-server/lesson-29/server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  if(url === "/" && method === "GET") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(path.join(__dirname, "pages", "home.html")).pipe(res);
+  } else if (url === "/contact" && method === "GET") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(path.join(__dirname, "pages", "contact.html")).pipe(res);
+  } else if (url === "/about" && method === "GET") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(path.join(__dirname, "pages", "about.html")).pipe(res);
+  } else {
+    res.writeHead(404, {"Content-Type": "text/html"});
+    fs.createReadStream(path.join(__dirname, "pages", "404.html")).pipe(res);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server start")
+})
