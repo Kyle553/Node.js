3d497211933baa13098fed291a0108a2c14ea0d6
Kyle553 <elkorda204@gmail.com>
2025-10-22 14:34:52 +0300

    lesson-30-(not-finished)

--- node.js/module-7-node-js-server/lesson-30/server.js
+++ node.js/module-7-node-js-server/lesson-30/server.js
@@ -1,27 +1,60 @@
 const http = require("http");
-const url = require("url");
 const path = require("path");
 const fs = require("fs");
 
+const folders = {
+  js: "js",
+  html: "pages",
+  css: "styles"
+}
+const contentType = {
+  js: {"Content-Type": "text/javascript"},
+  html: {"Content-Type": "text/html"},
+  css: {"Content-Type": "text/css"}
+}
+let filePathValue = "";
+
+function readFile(filePath, res, contentType) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error("ERROR:\n", err);
+
+      res.writeHead(500, {"Content-Type": "text/plain"});
+      res.end("Server error");
+      return;
+    }
+
+    res.writeHead(200, contentType);
+    res.end(data);
+  })
+}
+
+function filePath(folder, fileName) {
+  return path.join(__dirname, folder, fileName);
+}
+
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  const baseUrl = "http://localhost:3000"
+  const parsedUrl = new URL(req.url, baseUrl);
 
   if (parsedUrl.pathname === "/" && req.method === "GET") {
-    const filePath = path.join(__dirname, "pages", "home.html")
-    // const stream = fs.createReadStream(filePath);
-    
-    // stream.on("error", (err) => {
-    //   console.error("ERROR:\n", err);
+    filePathValue = filePath(folders.html ,"home.html");
+    readFile(filePathValue, res, contentType.html);
 
-    //   res.writeHead(500, {"Content-Type": "text/plain"});
-    //   res.end("Server error");
-    // })
+  } else if (parsedUrl.pathname === "/styles/home.css") {
+    filePathValue = filePath(folders.css, "home.css");
+    readFile(filePathValue, res, contentType.css);
 
-    // res.writeHead(200, {"Content-Type": "text/html"});
-    // stream.pipe(res);
+  } else if (parsedUrl.pathname === "/js/home.js") {
+    filePathValue = filePath(folders.js, "home.js");
+    readFile(filePathValue, res, contentType.js);
+
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end("Page not found");
   }
 });
 
 server.listen(3000, () => {
   console.log("Server started")
-});
+}); 
